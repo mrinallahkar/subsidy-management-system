f71b7d1a9c3bd622eb8f97f052a2e7965f926547
mrinallahkar <2022MT93071@wilp.bits-pilani.ac.in>
2024-05-14 12:39:27 +0530

    Updating Footer file

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container-fluid clearfix">
     <span class="text-muted d-block text-center text-sm-left d-sm-inline-block"></span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright ©  <a href="http://www.nedfi.com/" target="_blank">NEDFi</a> All rights reserved.</i>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright ©  <a href="http://www.nedfi.com/" target="_blank">NEDFi @2024</a> All rights reserved.</i>
     </span>
   </div>
 </footer>
